cd63f9b9697a875645619413960ad5d01ccbe039
utroyasnoe <93712879+utroyasnoe@users.noreply.github.com>
2023-05-30 08:17:06 +1000

    Add files via upload

---
+++ ChkNetworkStatus/Source/CheckIPPort1.py
@@ -0,0 +1,17 @@
+__author__ = "Your name"
+__email__ = "yourname@studytafensw.edu.au"
+__copyright__ = "Copyright Gelos Enterprise"
+__license__ = "Proprietary"
+__last_update_date__ = "00/00/0000"
+__version__ = "1.0.1"
+__status__ = "Development"
+
+"""Describe the program here in few sentences"""
+
+# Imports
+
+
+# Functions
+
+
+# Program
